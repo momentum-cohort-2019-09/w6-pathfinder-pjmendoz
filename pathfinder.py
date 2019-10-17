2bdad69d46173ae5b339d9590bcc80089a0f2396
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-17 17:58:32 -0400

    able to grab one individual pixel

--- pathfinder.py
+++ pathfinder.py
@@ -1,17 +1,37 @@
 from PIL import Image
 
-im = Image.new('RGBA', (600, 600), 'black')
+im = Image.new('RGB', (600, 600), 'black')
 im.save('map.png')
 
 def read_word_list(filename): 
+
     with open(filename) as file:   
-        data = file.read().split()
-        
+        data = file.read()
+        data_points = [[int(x) for x in line.split()] for line in data.split("\n")]
+
+    min = data_points[0][0]
+    max = data_points[0][0]
+
+    for x in data_points: 
+        for integer in x:   
+            if integer < min: 
+                min = integer
+            if integer > max: 
+                max = integer           
+
+    colors_big_list = []
+    little_rows_of_colors = []
+
+    for rows in data_points: 
+        for number in rows: 
+            color_int = round(((number - min) / (max-min)) * 255)
+            little_rows_of_colors.append(color_int)    
+        colors_big_list.append(little_rows_of_colors)
+        little_rows_of_colors = []
+
+    print(colors_big_list[0][0])
+    print(len(colors_big_list[0]))       
     return data
 
+read_word_list("elevation_small.txt")    
 
-print(read_word_list("elevation_small.txt"))
-
-# class Map: 
-
-#     def _init_(self): 
