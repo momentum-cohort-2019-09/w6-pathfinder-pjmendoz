153d50c9d00bf0f053c8109ee0550f8e67e45582
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-18 15:03:22 -0400

    getting an image, but something is not working

--- pathfinder.py
+++ pathfinder.py
@@ -1,48 +1,57 @@
+import math
 from PIL import Image
+import numpy as np
 
 class Map: 
-
-    def _init_(self, file):
+    def __init__(self, file):
         self.file = file
+        self.elevations = []
+        self.min_elevation = []
+        self.max_elevation = []
+        self.text_contents = []
+        self.colors_big_list = []
+        self.little_rows_of_colors = []
 
-    def read_file(self, file): 
+    def read_file(self): 
+        with open(self.file) as text_file:   
+            self.text_contents = text_file.read()
 
-        with open(file) as text_file:   
-            text_contents = text_file.read()
-
-        self.elevations = [[int(x) for x in line.split()] for line in text_contents.split("\n")]
+    def find_elevations(self):
+        self.elevations = [[int(each) for each in line.split()] for line in self.text_contents.split("\n")]
     
     def find_min_and_max(self):
 
-        min = self.elevations[0][0]
-        max = self.elevations[0][0]
+        self.min_elevation = self.elevations[0][0]
+        self.max_elevation = self.elevations[0][0]
 
-        for x in self.elevations: 
-            for integer in x:   
-                if integer < min: 
-                    min = integer
-                if integer > max: 
-                    max = integer           
+        for each in self.elevations: 
+            for integer in each:   
+                if integer < self.min_elevation: 
+                    self.min_evaluation = integer
+                if integer > self.max_elevation: 
+                    self.max_evaluation = integer           
 
-    def create_color_list(self):
-        colors_big_list = []
-        little_rows_of_colors = []
+    def get_colors_from_elevations(self):
 
         for rows in self.elevations: 
             for number in rows: 
-                color_int = round(((number - min) / (max-min)) * 255)
-                little_rows_of_colors.append(color_int)    
-            colors_big_list.append(little_rows_of_colors)
-            little_rows_of_colors = []
+                color_int = round(((number - self.min_evaluation) / (self.max_evaluation - self.min_evaluation)) * 255)
+                self.little_rows_of_colors.append(color_int)    
+            self.colors_big_list.append(self.little_rows_of_colors)
+            self.little_rows_of_colors = []
      
     def create_map_image(self):
-        im = Image.new('RGB', (600, 600), 'black')
-        im.save('map.png')
-
+        img = Image.fromarray(np.uint8(self.colors_big_list))
+        img.save('test.png')
 
 if __name__ == "__main__":
     map = Map("elevation_small.txt")
-
+    map.read_file()
+    map.find_elevations()
+    map.find_elevations()
+    map.find_min_and_max()
+    map.get_colors_from_elevations()
+    map.create_map_image()
 
       
 
