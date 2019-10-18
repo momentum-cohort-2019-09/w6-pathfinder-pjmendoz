49854da5ee8cf98e4386691bf74c75d42e0e99bf
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-18 09:25:43 -0400

    changing little_rows_of_colors to brightness

--- practice.py
+++ practice.py
@@ -4,7 +4,7 @@ from PIL import Image
 with open('elevation_small.txt') as text_file:   
         text_contents = text_file.read()
 
-        elevations = [[int(x) for x in line.split()] for line in text_contents.split("\n")]
+        elevations = [[int(each) for each in line.split()] for line in text_contents.split("\n")]
 
         map_height = (len(elevations))
         map_width = (len(elevations))
@@ -12,22 +12,22 @@ with open('elevation_small.txt') as text_file:
         min = elevations[0][0]
         max = elevations[0][0]
 
-        for x in elevations: 
-            for integer in x:   
+        for each in elevations: 
+            for integer in each:   
                 if integer < min: 
                     min = integer
                 if integer > max: 
                     max = integer           
 
         colors_big_list = []
-        little_rows_of_colors = []
+        brightness = []
 
         for rows in elevations: 
             for number in rows: 
                 color_int = round(((number - min) / (max-min)) * 255)
-                little_rows_of_colors.append(color_int)    
-            colors_big_list.append(little_rows_of_colors)
-            little_rows_of_colors = []
+                brightness.append(color_int)    
+            colors_big_list.append(brightness)
+            brightness = []
      
         im = Image.new('RGB', (map_width, map_height), 'black')
         im.save('map.png')
